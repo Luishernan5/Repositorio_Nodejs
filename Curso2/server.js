23d43096f6c2fcde3e33d6c2a0463b40fe80fe88
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-25 18:16:47 -0600

    add

--- Curso2/server.js
+++ Curso2/server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 http.createServer((req,res) =>{
-    res.write('Hola servidor node.');
+    res.write('Hola servidor node');
     res.end()
 }).listen(3000)
 console.log('El servidor esta listo en el puerto 3000');
